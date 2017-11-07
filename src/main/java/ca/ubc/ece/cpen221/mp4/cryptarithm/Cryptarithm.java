19cba86cc44b35d5ff23f217c9e18798f6e9cb8b
Nancy Jiang <nancyjiang10@gmail.com>
2017-11-06 22:02:55 -0800

    did some work on cryptarithm parser

--- src/main/java/ca/ubc/ece/cpen221/mp4/cryptarithm/Cryptarithm.java
+++ src/main/java/ca/ubc/ece/cpen221/mp4/cryptarithm/Cryptarithm.java
@@ -3,11 +3,79 @@ package ca.ubc.ece.cpen221.mp4.cryptarithm;
 import java.util.List;
 import java.util.Map;
 
+import ca.ubc.ece.cpen221.mp4.expression.NonVariableExpression;
+import ca.ubc.ece.cpen221.mp4.expression.VariableExpression;
+import ca.ubc.ece.cpen221.mp4.operator.Addition;
+import ca.ubc.ece.cpen221.mp4.operator.BinaryOperator;
+import ca.ubc.ece.cpen221.mp4.operator.Division;
+import ca.ubc.ece.cpen221.mp4.operator.Multiplication;
+import ca.ubc.ece.cpen221.mp4.operator.Operator;
+import ca.ubc.ece.cpen221.mp4.operator.Subtraction;
+
 /**
  * Cryptarithm - a datatype that represents a cryptarithm
  *
  */
 public class Cryptarithm {
+	private NonVariableExpression lhs;
+	private NonVariableExpression rhs;
+	private List<VariableExpression> letters;
+
+	/**
+	 * 
+	 */
+	private BinaryOperator OperationConstructor(String s) {
+		BinaryOperator op = null;
+		switch (s) {
+		case "+":
+			op = new Addition();
+			break;
+		case "-":
+			op = new Subtraction();
+			break;
+		case "*":
+			op = new Multiplication();
+			break;
+		case "/":
+			op = new Division();
+			break;
+		// can use if we want to check for invalid operations
+		// default: throw new NoSolutionException();
+		}
+		return op;
+	}
+
+	/**
+	 * 
+	 */
+	private NonVariableExpression WordConstructor(String word, int i) {
+		VariableExpression letter;
+		VariableExpression magnitude = new VariableExpression("magnitude");
+		BinaryOperator op = null;
+		NonVariableExpression parsedWord = null;
+
+		if (i % 2 == 1) {
+			int iterator = word.length() - 1;
+			for (char c : word.toCharArray()) {
+				magnitude.store(Math.pow(10, iterator--));
+				letter = new VariableExpression(String.valueOf(c));
+
+				if (!letters.contains(letter)) {
+					letters.add(letter);
+				}
+				parsedWord = new NonVariableExpression(new Multiplication(), letter, magnitude);
+
+				if (op == null)
+					lhs = parsedWord;
+				else
+					lhs = new NonVariableExpression(op, lhs, parsedWord);
+			}
+		} else {
+			op = (OperationConstructor(word));
+		}
+		return parsedWord;
+
+	}
 
 	/**
 	 * Cryptarithm constructor
@@ -16,16 +84,46 @@ public class Cryptarithm {
 	 *            where each element is a String that represents part of the
 	 *            cryptarithm
 	 */
+	// ** CAN WE ASSUME THAT THEY PASS A VALID CRYPTARITHM?
 	public Cryptarithm(String[] cryptarithm) {
-		// TODO implement this constructor
+		NonVariableExpression word;
+		VariableExpression letter;
+		VariableExpression magnitude = new VariableExpression("mag");
+		BinaryOperator op = null;
+
+		for (int i = 0; cryptarithm[i] != "="; i++) {
+			if (i % 2 == 1) {
+				int iterator = cryptarithm[i].length() - 1;
+				for (char c : cryptarithm[i].toCharArray()) {
+					magnitude.store(Math.pow(10, iterator--));
+					letter = new VariableExpression(String.valueOf(c));
+
+					if (!letters.contains(letter)) {
+						letters.add(letter);
+					}
+					word = new NonVariableExpression(new Multiplication(), letter, magnitude);
+
+					if (op == null)
+						lhs = word;
+					else
+						lhs = new NonVariableExpression(op, lhs, word);
+				}
+			} else {
+				op = (OperationConstructor(cryptarithm[i]));
+			}
+		}
+
+		for (int i = cryptarithm.length - 1; cryptarithm[i] != "="; i--) {
+
+		}
 	}
 
 	/**
 	 * Find solutions to the cryptarithm
 	 * 
-	 * @return a list of all possible solutions to the given cryptarithm. A
-	 *         solution is a map that provides the value for each alphabet in
-	 *         the cryptarithm.
+	 * @return a list of all possible solutions to the given cryptarithm. A solution
+	 *         is a map that provides the value for each alphabet in the
+	 *         cryptarithm.
 	 */
 	public List<Map<Character, Integer>> solve() throws NoSolutionException {
 		// TODO implement this method
