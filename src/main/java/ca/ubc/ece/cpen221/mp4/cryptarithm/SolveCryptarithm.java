616d1d1d11a722f1bfcfc4822ae2d95b5676dd43
Nancy <nancyjiang10@gmail.com>
2017-11-13 02:06:17 -0800

    one more

--- src/main/java/ca/ubc/ece/cpen221/mp4/cryptarithm/SolveCryptarithm.java
+++ src/main/java/ca/ubc/ece/cpen221/mp4/cryptarithm/SolveCryptarithm.java
@@ -7,24 +7,27 @@ import java.util.Scanner;
 public class SolveCryptarithm {
 
 	public static void main(String[] args) {
-		//scan a line inputed by the user
+		String expression;
+		
+		// scan a line inputed by the user
+		@SuppressWarnings("resource")
 		Scanner scanner = new Scanner(System.in);
-		System.out.println("Enter a cryptarithm:");
-		String expression = scanner.nextLine();
-		scanner.close();
-		//split line into strings to pass to array
-		String[] cryptos = expression.split(" ");
-		try {
-			//try to solve cryptarithm 
-			Cryptarithm crypto = new Cryptarithm(cryptos);
-			List<Map<Character,Integer>> solutions = crypto.solve();
-			System.out.println("Found " + solutions.size() + " solution(s):");
-			System.out.println(solutions);
-		} catch (InvalidCryptarithmException e) {
-			System.out.println("Invalid input");
-		}catch(NoSolutionException e) {
-			System.out.println("No solution for the cryptorithm");
+		while(true){
+			System.out.println("Enter a cryptarithm:");
+			expression = scanner.nextLine();
+			// split line into strings to pass to array
+			String[] cryptos = expression.split(" ");
+			try {
+				// try to solve cryptarithm
+				Cryptarithm crypto = new Cryptarithm(cryptos);
+				List<Map<Character, Integer>> solutions = crypto.solve();
+				System.out.println("Found " + solutions.size() + " solution(s):");
+				System.out.println(solutions + "\n");
+			} catch (InvalidCryptarithmException e) {
+				System.out.println("Invalid input");
+			} catch (NoSolutionException e) {
+				System.out.println("No solution for the cryptorithm");
+			}
 		}
 	}
-	
 }
